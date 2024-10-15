f6cb4b4da32642b0aadc9dc82bfa8fefc124a1f7
Victor-Pedroso <victor.duma@zopone.com.br>
2024-10-15 08:51:58 -0300

    Configurações Iniciais

--- src/main.js
+++ src/main.js
@@ -1,3 +1,8 @@
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 import { createApp } from 'vue'
 import App from './App.vue'
 
